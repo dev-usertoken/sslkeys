141ce456e7a20a65f13054a4cf61668d39138df4
hackathon <hack@Authorizeds-MacBook-Air-3.local>
2015-04-17 16:37:02 -0700

    updated

--- crypto5/server.js
+++ crypto5/server.js
@@ -6,6 +6,8 @@ var MongoClient = require('mongodb').MongoClient
 var url = process.env.MongoDB || 'mongodb://localhost:27017/keys';
 var count = process.env.Counter || 10;
 var dropCollection = process.env.DropCollection || 0;
+var counterStart = process.env.CounterStart || 0;
+var counterEnd = process.env.CounterEnd || 10;
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
@@ -15,7 +17,7 @@ MongoClient.connect(url, function(err, db) {
 
   if(dropCollection) col.drop();
 
-  for(var i = 0; i < count; i++) {
+  for(var i = counterStart; i < counterEnd; i++) {
     var string = e.randomAsciiString(256);
     var query = {tokenId: i, token: string}
     bulk.insert(query);
